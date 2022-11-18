bf83f2e5cd45108e206dcf586ad7024ed255ffac
Stalkakuma <stalkakuma@gmail.com>
2022-11-18 09:58:52 +0200

    make list items toggle

--- components/pages/guidepage/step.tsx
+++ components/pages/guidepage/step.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import {
   VStack,
-  HStack,
   Heading,
   Text,
   Link as ChakraLink,
-  ListIcon,
   Box,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import GuideImage from "./guide-image";
 
@@ -44,48 +45,53 @@ const Step: FC<StepProps> = ({
   imageTwoAlt,
 }) => {
   return (
-    <VStack align={"start"}>
-      <HStack>
-        <ListIcon as={ChevronDownIcon} />
-        <Heading as={"h3"}>{title}</Heading>
-      </HStack>
-      <Text alignSelf={"center"}>
-        {textOne}
-        {linkOne && (
-          <ChakraLink
-            isExternal={true}
-            color={"blue.400"}
-            as={Link}
-            href={linkOne}
-          >
-            {linkOneName}
-          </ChakraLink>
+    <AccordionItem border={"none"} align={"start"} as={VStack}>
+      <Box ml={-5}>
+        <AccordionButton>
+          <AccordionIcon />
+          <Heading as={"h3"}>{title}</Heading>
+          <AccordionIcon />
+        </AccordionButton>
+      </Box>
+      <AccordionPanel align={"start"} w={"100%"} as={VStack}>
+        <Text alignSelf={"center"}>
+          {textOne}
+          {linkOne && (
+            <ChakraLink
+              isExternal={true}
+              color={"blue.400"}
+              as={Link}
+              href={linkOne}
+            >
+              {linkOneName}
+            </ChakraLink>
+          )}
+          {textTwo}
+          {linkTwo && (
+            <ChakraLink
+              isExternal={true}
+              color={"blue.400"}
+              as={Link}
+              href={linkTwo}
+            >
+              {linkTwoName}
+            </ChakraLink>
+          )}
+          {textThree}
+        </Text>
+        {imageOne && (
+          <Box alignSelf={"center"}>
+            <GuideImage imageLocation={imageOne} imageAlt={imageOneAlt} />
+          </Box>
         )}
-        {textTwo}
-        {linkTwo && (
-          <ChakraLink
-            isExternal={true}
-            color={"blue.400"}
-            as={Link}
-            href={linkTwo}
-          >
-            {linkTwoName}
-          </ChakraLink>
+        {additionalTextOne && <Text>{additionalTextOne}</Text>}
+        {imageTwo && (
+          <Box alignSelf={"center"}>
+            <GuideImage imageLocation={imageTwo} imageAlt={imageTwoAlt} />
+          </Box>
         )}
-        {textThree}
-      </Text>
-      {imageOne && (
-        <Box alignSelf={"center"}>
-          <GuideImage imageLocation={imageOne} imageAlt={imageOneAlt} />
-        </Box>
-      )}
-      {additionalTextOne && <Text>{additionalTextOne}</Text>}
-      {imageTwo && (
-        <Box alignSelf={"center"}>
-          <GuideImage imageLocation={imageTwo} imageAlt={imageTwoAlt} />
-        </Box>
-      )}
-    </VStack>
+      </AccordionPanel>
+    </AccordionItem>
   );
 };
 
--- pages/guide.tsx
+++ pages/guide.tsx
@@ -1,4 +1,11 @@
-import { Container, List, ListItem, Heading, Text } from "@chakra-ui/react";
+import {
+  Container,
+  List,
+  ListItem,
+  Heading,
+  Text,
+  Accordion,
+} from "@chakra-ui/react";
 import Layout from "components/layout";
 import Step from "components/pages/guidepage/step";
 
@@ -15,65 +22,68 @@ const Guide = () => {
         <Heading as={"h2"} py={10}>
           {"Let's setup Minecraft!"}
         </Heading>
-        <List spacing={8}>
-          <ListItem>
-            <Step
-              title={"Install or Update your TLauncher!"}
-              textOne={"If you don't already have it, here's a "}
-              linkOne={"https://tlauncher.org/en/"}
-              linkOneName={"link"}
-            />
-          </ListItem>
-          <ListItem>
-            <Step
-              title={"Login to your account!"}
-              textOne={
-                "Before we continue, TLauncher is going to ask you to login most likely. I personally use microsoft account, because Minecraft is cheap and awesome and most importantly that way you can use skins! You are free to use TLauncher account otherwise."
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <Step
-              title={"Install Fabric 1.19.2!"}
-              textOne={
-                "In your TLauncher select Fabric and install it. Fabric will help us run a few mods. For clarity, I'm adding a picture bellow:"
-              }
-              imageOne={"/minecraft1.png"}
-              imageOneAlt={"Fabric"}
-            />
-          </ListItem>
-          <ListItem>
-            <Step
-              title={"Download Mods!"}
-              textOne={
-                "I understand that not everyone wants to mod, but these two mods are for PERFORMANCE. Yes, up to 200% better fps! It will especially help if you are playing on potato laptop. Here are links to "
-              }
-              linkOne={
-                "https://www.curseforge.com/minecraft/mc-mods/optifabric/download"
-              }
-              linkOneName={"OptiFabric"}
-              textTwo={" and "}
-              linkTwo={"https://optifine.net/downloads"}
-              linkTwoName={" Optifine"}
-              textThree={
-                ". Just click download on both of them and we will use them in the next step."
-              }
-            />
-          </ListItem>
-          <ListItem>
-            <Step
-              title={"Install mods!"}
-              textOne={
-                "In your TLauncher click on a open folder icon( I'm attaching a picture bellow for clarity ). This will open your Minecraft folder. Once there Navigate to a folder named mods and move the TWO FILES we just downloaded there."
-              }
-              imageOne={"/minecraft2.png"}
-              imageOneAlt={"Folder"}
-              additionalTextOne={"End result should look like this:"}
-              imageTwo={"/minecraft3.png"}
-              imageTwoAlt={"End result"}
-            />
-          </ListItem>
-        </List>
+        <Accordion defaultIndex={[0, 1, 2, 3, 4]} allowMultiple>
+          <List spacing={8}>
+            <ListItem>
+              <Step
+                title={"Install or Update your TLauncher!"}
+                textOne={"If you don't already have it, here's a "}
+                linkOne={"https://tlauncher.org/en/"}
+                linkOneName={"link"}
+              />
+            </ListItem>
+            <ListItem>
+              <Step
+                title={"Login to your account!"}
+                textOne={
+                  "Before we continue, TLauncher is going to ask you to login most likely. I personally use microsoft account, because Minecraft is cheap and awesome and most importantly that way you can use skins! You are free to use TLauncher account otherwise."
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <Step
+                title={"Install Fabric 1.19.2!"}
+                textOne={
+                  "In your TLauncher select Fabric and install it. Fabric will help us run a few mods. For clarity, I'm adding a picture bellow:"
+                }
+                imageOne={"/minecraft1.png"}
+                imageOneAlt={"Fabric"}
+              />
+            </ListItem>
+            <ListItem>
+              <Step
+                title={"Download Mods!"}
+                textOne={
+                  "I understand that not everyone wants to mod, but these two mods are for PERFORMANCE. Yes, up to 200% better fps! It will especially help if you are playing on potato laptop. Here are links to "
+                }
+                linkOne={
+                  "https://www.curseforge.com/minecraft/mc-mods/optifabric/download"
+                }
+                linkOneName={"OptiFabric"}
+                textTwo={" and "}
+                linkTwo={"https://optifine.net/downloads"}
+                linkTwoName={" Optifine"}
+                textThree={
+                  ". Just click download on both of them and we will use them in the next step."
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <Step
+                title={"Install mods!"}
+                textOne={
+                  "In your TLauncher click on a open folder icon( I'm attaching a picture bellow for clarity ). This will open your Minecraft folder. Once there Navigate to a folder named mods and move the TWO FILES we just downloaded there."
+                }
+                imageOne={"/minecraft2.png"}
+                imageOneAlt={"Folder"}
+                additionalTextOne={"End result should look like this:"}
+                imageTwo={"/minecraft3.png"}
+                imageTwoAlt={"End result"}
+              />
+            </ListItem>
+          </List>
+        </Accordion>
+
         <Text py={7}>
           {
             "For the basic guide, this should be it! You can play Minecraft now with hopefully a good performance. The next steps are OPTIONAL."
